df19f5ec467c424040d4026ddf9c4e03e9908872
krishayPradeep <68918227+krishayPradeep@users.noreply.github.com>
2020-08-08 15:13:50 -0700

    initial xcode project creation

---
+++ youtubeOneDay_Build/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtubeOneDay_Build
+//
+//  Created by Krishay Pradeep on 2020-08-08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
